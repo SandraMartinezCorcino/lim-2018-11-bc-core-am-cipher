2f5cec05a7e7f8b701dd511739c5110f3e31b04e
SandraMartinezCorcino <sandra.martinez.corcino@gmail.com>
2018-11-19 18:38:18 -0500

    SE COMPLETO LA FUNCION ENCODE Y DECODE DEL ARCHIVO CIPHER.JS EL CUAL VALIDA ESPACIOS BLANCO Y EN LA FUNCION DECODE SE UTILIZA LA LOGIGA DE MAYOR A 26

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,79 @@
 window.cipher = {
 
  encode: function(offset, string){
-  
-  const positionAscii=[] ;
-  
-  for (var i = 0; i < $cifrar.length ; i++) {
 
-    console.log($offset);
-    $positionAscii[i] = String.fromCharCode(($cifrar.charCodeAt(i)  - 65 + $offset) % 26 + 65) ;
-}
-  console.log(positionAscii);
+
+  
+          const positionAscii=[] ;
+
+          const codeAscci=[];
+
+          const offset1=parseInt(offset);
+  
+          for (var i = 0; i < string.length ; i++) {
+            
+            codeAscci[i]=string.toUpperCase().charCodeAt(i);
+
+            if (codeAscci[i]==32) {
+              positionAscii[i]=' ';
+            } else {
+              
+              positionAscii[i] = String.fromCharCode((codeAscci[i]  - 65 + offset1) % 26 + 65) ;
+
+            }
+          
+          }
+          
+          
+          return positionAscii.join('');
+         
  },
 
  decode:function(offset, string){
-  console.log('objetodecode');
+
+          const positionAscii=[] ;
+
+          const offset1=parseInt(offset);
+
+          const codeAscci=[];
+
+
+
+          for (var i = 0; i < string.length ; i++) {
+
+              codeAscci[i]=string.toUpperCase().charCodeAt(i);
+
+
+              if (codeAscci[i]==32) {
+
+                positionAscii[i]=' ';
+              
+              } else {
+                console.log(offset1);
+                console.log(codeAscci[i]);
+                console.log(codeAscci[i]-65);
+                console.log(codeAscci[i]-65 -offset1 );
+
+                const conditionForm= Math.abs( codeAscci[i]-65 -offset1) ;
+
+                console.log(conditionForm);
+
+                if (conditionForm <= 26 ) {
+                  
+                  positionAscii[i] = String.fromCharCode(  ( codeAscci[i]-65) - offset1  % 26 + 65) ;
+
+                } else  {
+                  positionAscii[i]= String.fromCharCode(  ( codeAscci[i]-65) - offset1  % 26 + 91 );
+                }
+
+
+               
+
+              }
+          }
+  
+          return positionAscii.join('');
+  
 
  }
 };
--- src/index.js
+++ src/index.js
@@ -13,46 +13,51 @@ const sectionDecrypt=document.getElementById('containerDecrypt');
 //  --Hoja Encode--
 const buttonEncode=document.getElementById('btnEncode');
 
+// -- Hoja Decode --
 
+const buttonDecode=document.getElementById('btnDecode');
 //==================================================================
-//ASOCIAR EL EVENTO A ESE ELEMENTO
-
-// -- Hoja Home --
-buttonEncrypt.addEventListener('click',funcEncrypt);
-buttonDecrypt.addEventListener('click',funcDecrypt);
-
-//  --Hoja Encode--
-buttonEncode.addEventListener('click',funEncode);
-
-//==================================================================
-//EVENTO
+///ASOCIAR EL EVENTO A ESE ELEMENTO
 
 sectionDecrypt.style.display='none';
 sectionEncrypt.style.display='none';
 
 // -- Hoja Home --
-function funcEncrypt() {
+
+buttonEncrypt.addEventListener( 'click',() => {
     sectionSidebar.style.display='none' ;  
     sectionHome.style.display='none';
     sectionDecrypt.style.display='none';
     sectionEncrypt.style.display='block';
-}
-
-function funcDecrypt(){
+});
+buttonDecrypt.addEventListener('click',() => {
     sectionSidebar.style.display='none';
     sectionHome.style.display='none';
     sectionEncrypt.style.display='none';
     sectionDecrypt.style.display='block';
-
-}
+});
 
 //  --Hoja Encode--
-function funEncode(){
+    
+//buttonEncode.addEventListener('click',funEncode);
 
+
+buttonEncode.addEventListener('click',() => {
     const offsetEncode=document.getElementById('offset').value;
     const messageEncode=document.getElementById('messageEncode').value;
+    const messageResult=document.getElementById('message-result');
 
-    cipher.encode(offsetEncode,messageEncode);
-}
+    messageResult.innerHTML=cipher.encode(offsetEncode,messageEncode);
+});
+
+// -- Hoja Decode
+
+buttonDecode.addEventListener('click',() => {
+    
+    const offsetDecode=document.getElementById('offsetDecode').value;
+    const messageDecode = document.getElementById('messageDecode').value;
+    const messageResultDecode=document.getElementById('message-result-decode');
+    messageResultDecode.innerHTML=cipher.decode(offsetDecode,messageDecode);
+} );
 
 //==================================================================
