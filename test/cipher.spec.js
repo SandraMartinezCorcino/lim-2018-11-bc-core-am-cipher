5df494467a9840f9c66cd99fdb78a83a25fcd258
SandraMartinezCorcino <sandra.martinez.corcino@gmail.com>
2018-11-27 18:23:51 -0500

    test

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,8 +10,17 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
-                          
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',() =>{
+      assert.equal( cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ') ,'HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+    
+    it('debería retornar espacio en blanco "HIJK LMN" para "ABCD EFG" con offest 33',() =>{
+      assert.equal( cipher.encode(33,'ABCD EFG') ,'HIJK LMN');
+    });
+
+    it( 'deria retornar en mayuscula "HIJK" para "abcd" con offset 33 ',()=>{
+      assert.equal(  cipher.encode(33,'abcd') , 'HIJK' );
+    });
 
   });
 
@@ -21,11 +30,22 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33' , ()=>{
+
+      assert.equal( cipher.decode( 33,'HIJKLMNOPQRSTUVWXYZABCDEFG') , 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' );
+
+    });
+
+    it('deberia retorna espacio en blanco "ANA ANA " para "HUH HUH " con offest 33',()=>{
+
+      assert.equal( cipher.decode(33,'HUH HUH'),'ANA ANA' );
+    });
+
+    
                            
   });
 
-  describe('cipher.createCipherWithOffset', () => {
+ /* describe('cipher.createCipherWithOffset', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.createCipherWithOffset, 'function');
@@ -33,6 +53,6 @@ describe('cipher', () => {
 
     it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado');
 
-  });
+  });*/
 
 });
